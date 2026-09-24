67b7b0b34cf85abc081295bd6f149b277f7bb98e
Przemo <nowepk81@gmail.com>
2026-09-24 20:35:36 +0200

    Release version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android { namespace = "pl.erecruiter.bdmcoach"; compileSdk = 35
-    defaultConfig { applicationId = "pl.erecruiter.bdmcoach"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+    defaultConfig { applicationId = "pl.erecruiter.bdmcoach"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "1.1" }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
